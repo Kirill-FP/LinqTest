1deaa17ddd5fc7b2ce67abcd7eae4d34ae41a79e
Kirill Gonchar <kirill.gonchar@fluentpro.com>
2022-08-16 14:24:40 +0300

    completed

---
+++ LinqTest/GenerateTestData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest
+{
+    public static class GenerateTestData
+    {
+        public static List<ProjectTask> GenerateRandomAmountOfTestData(int tasksMaxNumberToCreate, int assignmentsMaxNumberToCreate, int resourcesMaxNumberToCreate)
+        {
+            Guid id;
+            int tasksNumberToCreate = new Random().Next(tasksMaxNumberToCreate / 2) + tasksMaxNumberToCreate / 2;
+            var projectTasks = new List<ProjectTask>();
+            var resourcePool = new List<Resource>();
+            for (int k = 0; k < resourcesMaxNumberToCreate; k++)
+            {
+                resourcePool.Add(new Resource() { ID = Guid.NewGuid(), Name = $"Resource{k}" });
+            }
+            for (int i = 0; i < tasksNumberToCreate; i++)
+            {
+                id = Guid.NewGuid();
+                int assignmentsNumberToCreate = new Random().Next(assignmentsMaxNumberToCreate / 2) + assignmentsMaxNumberToCreate / 2;
+                var currentTaskAssignments = new List<TaskAssignment>();
+                var startDate = DateTime.Now.AddDays((int)new Random().Next(10) - 5);
+                Resource? resource = null;
+                for (int j = 0; j < assignmentsNumberToCreate; j++)
+                    {
+                    for(int k = 0; k < resourcesMaxNumberToCreate; k++)
+                        {
+                            if (new Random().Next(500) == 5) //пусть редко какой таск будет иметь ассайнмент на ресурса, один из 500
+                                {
+                                    resource = resourcePool[k];
+                                    break;
+                                }
+                            else resource = null;
+                        }
+                    if (resource != null)
+                    {
+                        currentTaskAssignments
+                            .Add(new TaskAssignment
+                            {
+                                Id = Guid.NewGuid(),
+                                ParentID = id,
+                                Name = $"Task{i}, Assignment{j}",
+                                AssignedResource = resource
+                            });
+                    }
+                }
+                var currentTask = new ProjectTask()
+                {
+                    Id = id,
+                    Name = $"Task{i}",
+                    StartDate = new Random().Next(2) == 1 ? null : startDate, //пусть у половины тасков Start Date будет null
+                    FinishDate = startDate.AddDays((int)new Random().Next(10)),
+                    StoryPoints = 5,
+                    Assignments = currentTaskAssignments
+                };
+                projectTasks.Add(currentTask);
+            }
+            return projectTasks;
+        }
+    }
+}
--- LinqTest/GetTasksLINQ.cs
+++ LinqTest/GetTasksLINQ.cs
@@ -8,14 +8,52 @@ namespace LinqTest
 {
     public static class GetTasksLINQ
     {
-        public static IEnumerable<(Guid Id, string Name)>? Get10LatestTasks(IEnumerable<ProjectTask> projectTasksIEnumerable)
+        //0. получить последние 10 тасков расположеных в алфавитном порядке
+        //и вывести в виде именовоного tuple значения Ид и имени
+        public static IEnumerable<(Guid Id, string Name)> Get10LatestTasksLINQ(this IEnumerable<ProjectTask> projectTasksIEnumerable)
         {
-            var takeLast10projectTasks = projectTasksIEnumerable.
-                OrderBy(projectTask => projectTask.Name).
-                ThenByDescending(projectTask => projectTask.Name.Length).
-                TakeLast(10).
-                Select(projectTask => (projectTask.Id, projectTask.Name));
-            return projectTasksIEnumerable.Count() == 0 ? null : takeLast10projectTasks;
+            if (projectTasksIEnumerable == null)
+                return new List<(Guid, string)>();
+            return projectTasksIEnumerable
+                .OrderBy(_ => _.Name)
+                .ThenByDescending(_ => _.Name.Length)
+                .TakeLast(10)
+                .Select(_ => (_.Id, _.Name));
+            
+        }
+        //1. вычислить количество Late тасков
+        public static int GetLateTasksCountLINQ(this IEnumerable<ProjectTask> projectTasksIEnumerable)
+        {
+            return projectTasksIEnumerable.Where(_ => _.FinishDate < DateTime.Now).Count();
+        }
+
+        // 2. вычислить Late тaск который начался раньше всех
+        public static ProjectTask? GetLateTaskWithTheEarliestStartDateLINQ(this IEnumerable<ProjectTask> projectTasksIEnumerable)
+        {
+            return projectTasksIEnumerable
+                .Where(_ => _.FinishDate < DateTime.Now & _.StartDate != null)
+                .OrderBy(_ => _.StartDate).FirstOrDefault();
+        }
+        //3. вычислитиь список пользователей на которых заасайнены таски
+        public static List<Resource> GetResourcesWhoHaveAssignmentsToTasksLINQ(this IEnumerable<ProjectTask> projectTasksIEnumerable)
+        {
+            return projectTasksIEnumerable
+                .SelectMany(_ => _.Assignments)
+                .DistinctBy(_ => _.AssignedResource.Name)
+                .OrderBy(_ => _.AssignedResource.Name)
+                .Select(_ => _.AssignedResource)
+                .ToList();
+        }
+        // 4. вычислить сколько тасков имеет каждый юзер
+        public static IEnumerable<(Resource, int)> GetNubmerOfTasksForEachResourceLINQ(this IEnumerable<ProjectTask> projectTaskIEnumerable)
+        {
+            return projectTaskIEnumerable
+                .GetResourcesWhoHaveAssignmentsToTasksLINQ()
+                .Select(_ => (_, projectTaskIEnumerable
+                    .SelectMany(__ => __.Assignments)
+                    .Where(___ => ___.AssignedResource.Equals(_))
+                    .Count()
+                ));
         }
     }
 }
